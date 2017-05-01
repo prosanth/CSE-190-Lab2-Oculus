e3419683a10d3ae0e8d6ddbfbfdff754160e32c8
Prasanth Abraham <ppabraha@UCSD.EDU>
2017-05-01 15:27:57 -0700

    initial

---
+++ Minimal/Cube.cpp
@@ -0,0 +1,228 @@
+#include "Cube.h"
+#include "texture.h"
+#include <Windows.h>
+#include <iostream>
+
+Cube::Cube(bool skybox, bool left)
+{
+	if (!skybox) {
+		toWorld = glm::mat4(1.0f) *  glm::scale(glm::mat4(1.0f), glm::vec3(0.05f, 0.05f, 0.05f));
+		toWorld = toWorld * glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -10.0f));
+		setUpSkybox(skybox);
+	}
+	else {
+		toWorld = glm::mat4(1.0f);
+		if (left) {
+			loadBearLeftEye();
+		}
+		else {
+			loadBearRightEye();
+		}
+	}
+
+	this->skybox = skybox;
+
+	this->height = 30;
+	this->width = 30;
+
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+	
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+}
+
+Cube::~Cube()
+{
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
+}
+
+void Cube::draw(GLuint shaderProgram, glm::mat4 modelview, glm::mat4 projection)
+{
+	glm::mat4 newmv;
+	if (skybox) {
+		glDepthMask(GL_FALSE);
+
+		glEnable(GL_CULL_FACE);
+		glCullFace(GL_BACK);
+
+		newmv = glm::mat4(glm::mat3(modelview)) * toWorld;
+	}
+	else{
+		glEnable(GL_CULL_FACE);
+		glCullFace(GL_FRONT);
+
+		newmv = modelview * toWorld;
+	}
+	
+
+	uProjection = glGetUniformLocation(shaderProgram, "projection");
+	uModelview = glGetUniformLocation(shaderProgram, "modelview");
+
+	glUniformMatrix4fv(uProjection, 1, GL_FALSE, &projection[0][0]);
+	glUniformMatrix4fv(uModelview, 1, GL_FALSE, &newmv[0][0]);
+
+	glBindVertexArray(VAO);
+	glActiveTexture(GL_TEXTURE0);
+	glUniform1i(glGetUniformLocation(shaderProgram, "skybox"), 0);
+	glBindTexture(GL_TEXTURE_CUBE_MAP, textureID);
+	glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+	glBindVertexArray(0);
+
+	if (skybox) {
+		glDepthMask(GL_TRUE);
+	}
+}
+
+void Cube::setUpSkybox(bool skybox) {
+	glGenTextures(1, &textureID);
+	glBindTexture(GL_TEXTURE_CUBE_MAP, textureID);
+
+	unsigned char * right = loadPPM("./assets/vr_test_pattern.ppm", width, height);
+	glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, right);
+
+	unsigned char * left = loadPPM("./assets/vr_test_pattern.ppm", width, height);
+	glTexImage2D(GL_TEXTURE_CUBE_MAP_NEGATIVE_X, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, left);
+
+	unsigned char * top = loadPPM("./assets/vr_test_pattern.ppm", width, height);
+	glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_Y, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, top);
+
+	unsigned char * bottom = loadPPM("./assets/vr_test_pattern.ppm", width, height);
+	glTexImage2D(GL_TEXTURE_CUBE_MAP_NEGATIVE_Y, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, bottom);
+
+	unsigned char * back = loadPPM("./assets/vr_test_pattern.ppm", width, height);
+	glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_Z, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, back);
+
+	unsigned char * front = loadPPM("./assets/vr_test_pattern.ppm", width, height);
+	glTexImage2D(GL_TEXTURE_CUBE_MAP_NEGATIVE_Z, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, front);
+
+	// Make sure no bytes are padded:
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+
+	// Select GL_MODULATE to mix texture with polygon color for shading:
+	glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
+
+	// Use bilinear interpolation:
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	// Use clamp to edge to hide skybox edges:
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+	glBindTexture(GL_TEXTURE_CUBE_MAP, 0);
+}
+
+void Cube::loadBearRightEye() {
+	glGenTextures(1, &textureID);
+	glBindTexture(GL_TEXTURE_CUBE_MAP, textureID);
+
+	unsigned char * right = loadPPM("./assets/right-ppm/nx.ppm", width, height);
+	glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, right);
+
+	unsigned char * left = loadPPM("./assets/right-ppm/px.ppm", width, height);
+	glTexImage2D(GL_TEXTURE_CUBE_MAP_NEGATIVE_X, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, left);
+
+	unsigned char * top = loadPPM("./assets/right-ppm/py.ppm", width, height);
+	glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_Y, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, top);
+
+	unsigned char * bottom = loadPPM("./assets/right-ppm/ny.ppm", width, height);
+	glTexImage2D(GL_TEXTURE_CUBE_MAP_NEGATIVE_Y, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, bottom);
+
+	unsigned char * back = loadPPM("./assets/right-ppm/nz.ppm", width, height);
+	glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_Z, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, back);
+
+	unsigned char * front = loadPPM("./assets/right-ppm/pz.ppm", width, height);
+	glTexImage2D(GL_TEXTURE_CUBE_MAP_NEGATIVE_Z, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, front);
+
+	// Make sure no bytes are padded:
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+
+	// Select GL_MODULATE to mix texture with polygon color for shading:
+	glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
+
+	// Use bilinear interpolation:
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	// Use clamp to edge to hide skybox edges:
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+	glBindTexture(GL_TEXTURE_CUBE_MAP, 0);
+}
+
+void Cube::loadBearLeftEye() {
+	glGenTextures(1, &textureID);
+	glBindTexture(GL_TEXTURE_CUBE_MAP, textureID);
+
+	unsigned char * right = loadPPM("./assets/left-ppm/nx.ppm", width, height);
+	glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, right);
+
+	unsigned char * left = loadPPM("./assets/left-ppm/px.ppm", width, height);
+	glTexImage2D(GL_TEXTURE_CUBE_MAP_NEGATIVE_X, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, left);
+
+	unsigned char * top = loadPPM("./assets/left-ppm/py.ppm", width, height);
+	glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_Y, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, top);
+
+	unsigned char * bottom = loadPPM("./assets/left-ppm/ny.ppm", width, height);
+	glTexImage2D(GL_TEXTURE_CUBE_MAP_NEGATIVE_Y, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, bottom);
+
+	unsigned char * back = loadPPM("./assets/left-ppm/nz.ppm", width, height);
+	glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_Z, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, back);
+
+	unsigned char * front = loadPPM("./assets/left-ppm/pz.ppm", width, height);
+	glTexImage2D(GL_TEXTURE_CUBE_MAP_NEGATIVE_Z, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, front);
+
+	// Make sure no bytes are padded:
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+
+	// Select GL_MODULATE to mix texture with polygon color for shading:
+	glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
+
+	// Use bilinear interpolation:
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	// Use clamp to edge to hide skybox edges:
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+	glBindTexture(GL_TEXTURE_CUBE_MAP, 0);
+}
+
+void Cube::changeSize(bool increase, bool decrease, bool default) {
+	char buff[100];
+	sprintf_s(buff, "change size: %d\n", height);
+	OutputDebugStringA(buff);
+
+	if (increase && this->height < 80) {
+		toWorld = toWorld *  glm::scale(glm::mat4(1.0f), glm::vec3(1.05f, 1.05f, 1.05f));
+		this->height++;
+	}
+	else if(decrease && this->height >= 1) {
+		toWorld = toWorld *  glm::scale(glm::mat4(1.0f), glm::vec3(0.95f, 0.95f, 0.95f));
+		this->height--;
+	}
+	else if (default) {
+		toWorld = glm::mat4(1.0f) *  glm::scale(glm::mat4(1.0f), glm::vec3(0.05f, 0.05f, 0.05f));
+		toWorld = toWorld * glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -10.0f));
+		this->height = 30;
+	}
+
+}
+
